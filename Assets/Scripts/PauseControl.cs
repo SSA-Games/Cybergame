7c7d8281d9e9f916e2aec4afdd46457d609efcac
San4oGames <51251501+San4oGames@users.noreply.github.com>
2023-12-09 01:28:26 +0300

    1
    к

--- Assets/Scripts/PauseControl.cs
+++ Assets/Scripts/PauseControl.cs
@@ -1,28 +1,64 @@
+// PauseControl.cs
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PauseControl : MonoBehaviour
 {
-    public GameObject PauseUIObject;
-    public static bool isPaused; // Статус паузы
+    private static PauseUI_script pauseUIObject;
 
-    void Update()
+    void Start()
     {
-        //Проверка нажатия паузы каждый кадр
-        Pause();
+        pauseUIObject = GetComponent<PauseUI_script>();
     }
 
-    public static void Pause() //Логика паузы
+    public static void Pause(PauseUI_script uiObject)
     {
-        if (!isPaused && Input.GetKeyDown(KeyCode.Escape))
+        Time.timeScale = 0f;
+        if (uiObject != null)
         {
-            PauseUIObject.SetActive(true);
-            isPaused = true;
-            Time.timeScale = 0f;
+            uiObject.ShowPauseMenu();
         }
-        PauseUIObject.SetActive(false);
-        isPaused = false;
-        Time.timeScale = 1f;
+    }
+}
+
+// PauseUI_script.cs
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PauseUI_script : MonoBehaviour
+{
+    private VisualTreeAsset visualTreeAsset; // Добавлено поле для хранения шаблона UI
+    private VisualElement root;
+
+    void Awake()
+    {
+        visualTreeAsset = Resources.Load<VisualTreeAsset>("YourVisualTreeAssetName"); // Замените "YourVisualTreeAssetName" на фактическое имя вашего ресурса
+    }
+
+    public void OnEnable()
+    {
+        root = GetComponent<UIDocument>().rootVisualElement;
+
+        // Загрузка UI из VisualTreeAsset
+        VisualElement ui = visualTreeAsset.CloneTree();
+        root.Add(ui);
+
+        // Доступ к кнопкам
+        Button buttonResume = ui.Q<Button>("resume_button");
+        Button buttonOptions = ui.Q<Button>("options_button");
+        Button buttonExit = ui.Q<Button>("exit_button");
+
+        // Обработка событий
+        buttonResume.clicked += () => PauseControl.Pause(this);
+    }
+
+    public void ShowPauseMenu()
+    {
+        // Ваш код для отображения меню паузы
     }
 }
