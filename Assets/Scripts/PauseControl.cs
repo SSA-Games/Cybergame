e4ff17b63c740b0894b51ce7e91a897d93d5950e
Alex <kalininalexey2017@yandex.ru>
2023-12-09 01:37:24 +0300

    www

--- Assets/Scripts/PauseControl.cs
+++ Assets/Scripts/PauseControl.cs
@@ -5,23 +5,29 @@ using UnityEngine;
 public class PauseControl : MonoBehaviour
 {
     public GameObject PauseUIObject;
-    public static bool isPaused; // Статус паузы
+    public static bool isPaused = false; // Статус паузы
 
     void Update()
     {
         //Проверка нажатия паузы каждый кадр
         Pause();
+        if (!isPaused)
+        {
+            PauseUIObject.SetActive(false);
+        }
+        else
+        {
+            PauseUIObject.SetActive(true);
+        }
     }
 
     public static void Pause() //Логика паузы
     {
         if (!isPaused && Input.GetKeyDown(KeyCode.Escape))
         {
-            PauseUIObject.SetActive(true);
             isPaused = true;
             Time.timeScale = 0f;
         }
-        PauseUIObject.SetActive(false);
         isPaused = false;
         Time.timeScale = 1f;
     }
