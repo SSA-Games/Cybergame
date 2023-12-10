a7abaafb8bf17bea4dbb01b79d7f42748032957c
Alex <kalininalexey2017@yandex.ru>
2023-12-11 01:43:06 +0300

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,53 +2,82 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f; // Скорость игрока
-    public float jumpForce = 10f; // Сила прыжка
-    private bool isGrounded; // Переменная проверки соприкосновения
+    public float moveSpeed = 5f;
+    public float dashForce = 30f;
+    public float minJumpHeight = 2f;
+    public float maxJumpHeight = 3f;
+    public float maxJumpHoldTime = 1.0f;
 
+    private bool isGrounded;
     private Rigidbody2D rb;
     private Collider2D coll;
+    private bool isJumping = false;
+    private float jumpStartTime;
 
-
-    private void Start()
+    void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         coll = GetComponent<Collider2D>();
+        rb.freezeRotation = true;
     }
 
-    private void Update()
+    void Update()
     {
-        // Проверка соприкосновений
         isGrounded = Physics2D.IsTouchingLayers(coll, LayerMask.GetMask("Ground"));
 
-        if (!gameObject.GetComponent<PauseControl>().isPaused)
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            Move();
+            isJumping = true;
+            jumpStartTime = Time.time;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && isJumping)
+        {
+            Jump();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space) && isJumping)
+        {
+            isJumping = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Dash();
+        }
+    }
+
+    void Move()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+        rb.velocity = movement;
+
+        if (isJumping)
+        {
             Jump();
         }
     }
 
-    private void Move()
+    void Jump()
     {
-        // Горизонт -/+
-        float horizontalInput = Input.GetAxis("Horizontal");
-
-        // Вектор движения
-        Vector2 movement = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
-
-        // Применяес движение к Rb
-        rb.velocity = movement;
-
-    }
-
-    private void Jump()
-    {
-        // Нажата кнопка прыжка или нет и на земле ли игрок
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Time.time - jumpStartTime < maxJumpHoldTime)
         {
-            // Сила прыжка по вертикали
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            float jumpHeight = Mathf.Lerp(minJumpHeight, maxJumpHeight, Mathf.Clamp01((Time.time - jumpStartTime) / maxJumpHoldTime));
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
         }
     }
 
+    void Dash()
+    {
+        if (isGrounded)
+        {
+            rb.freezeRotation = false;
+            rb.AddForce(Vector2.down * 15f, ForceMode2D.Impulse);
+            rb.velocity = new Vector2(rb.velocity.x + dashForce, rb.velocity.y);
+            rb.freezeRotation = true;
+        }
+    }
 }
