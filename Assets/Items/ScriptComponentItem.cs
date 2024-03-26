0ba53fe3660b6dba7fb9b4c48e3c0a54c1c7679b
Alex <kalininalexey2017@yandex.ru>
2024-03-26 18:21:19 +0300

    something

--- Assets/Items/ScriptComponentItem.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Create new Item/Script Component")]
-public class ScriptComponentItem : Item
-{
-
-}
